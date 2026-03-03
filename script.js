265ecaa7db8bd4d1b921a0eddd8b729b27c5ab47
karianjahi <joseph@spiced-academy.com>
2026-03-03 19:43:06 +0100

    Initialize a regex with a default value that shall be modified in the future to serve a purpose

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
 let isError = false;
 const cleanInputString = (str) => {
-    
+    let regex = /hello/;
+
 };
+
