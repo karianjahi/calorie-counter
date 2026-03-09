1f09addfaa29f943eea662025941ab0bdbfb6db8
karianjahi <joseph@spiced-academy.com>
2026-03-09 16:56:19 +0100

    Capture inputs of type number inside the function

--- script.js
+++ script.js
@@ -47,4 +47,6 @@ function getCaloriesFromInputs(list) {
 function calculateCalories(e) {
     e.preventDefault();
     isError = false;
+    const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
 };
+
