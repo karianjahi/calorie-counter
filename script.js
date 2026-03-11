8bc60b2aa5772c02b8e170c2001ae2c2882b16bd
karianjahi <joseph@spiced-academy.com>
2026-03-11 18:37:41 +0100

    Calculate total number of calories

--- script.js
+++ script.js
@@ -56,12 +56,14 @@ function calculateCalories(e) {
     const dinnerNumberInputs = document.querySelectorAll("#dinner input[type='number']");
     const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
     const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
     const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
     const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
     const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
     const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
     const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
     if (isError) return null;
+    const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories
 };
 
 
